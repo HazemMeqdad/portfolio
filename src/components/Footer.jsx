54d6ddacda8fdfa1a0d1f5e9025483b54ef2b73a
hazemmeqdad <hazemmeqdad@outlook.com>
2023-11-21 15:12:23 +0300

    Footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
             </ul>
           </div>
         </nav>
-        <p>Copyright &#169; 2023 John Doe. All Rights Reserved.</p>
+        <p>Copyright &#169; 2023 HazemMeqdad. All Rights Reserved.</p>
       </footer>
         </>
     )
